fbe3bbef605e1ab503e06fc8500b79b9b74a9b72
jing1.liu <jing1.liu@momenta.ai>
2024-10-18 17:47:03 +0200

    18-10-24

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 //
 #include <iostream>
 #include <vector>
-#include "spiral_matrix_II_59.h"
+#include "squares_of_a_sorted_array_977_backup.h"
 
 using namespace std;
 
 int main() {
-    Solution s;
-    vector<int> nums = {2,3,1,2,4,3};
-    int n = 3;
-    int result = s.generateMatrix(n);
-    cout << result << endl;
+    Solution_2 s;
+    vector<int> nums = {-4, 3, 8, 18, 20};
+    vector<int> result = s.return_square_array(nums);
+    for(int i = 0; i < result.size(); ++i){
+        cout << result[i] << ' ';
+    }
     return 0;
 }
